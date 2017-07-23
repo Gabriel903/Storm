fa2141cde50ac83c0ecd063e79ac2cb194ca1258
Your Name <you@example.com>
2017-07-23 18:42:38 -0300

    v1.3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-$(document).ready(function(){
+jQuery( document ).ready(function($) {
 
-  $('.slider').slick();
+	var blogTitles = $('#negocios h2'),
+		$sidebar = $('#negocios .sidebar');
 
-  $('#negocios .slider').slick();
+	// Adicionar títulos dinamicamente
+	$.each(blogTitles, function( k, v ) {
+		$sidebar.append( '<span>' + $(v).text() + '</span>' );
+	});
+
+	// Ativar primeiro título
+	$sidebar.find('span:first-child').addClass('active');
+
+	// Iniciar slides
+	$('.slider, #negocios .slider').slick();
+
+	// Ao clicar nos botões do slide, mudar a cor do título
+  	$('#negocios .slider').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+  		$sidebar.find('span').removeClass('active');
+  		$sidebar.find('span:nth-child('+ (currentSlide + 1) +')').addClass('active');
+	});
  
 });
-		
